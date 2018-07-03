5208da3ea3a8bba29aabbeeb2af8aa5e297eebdf
Jose Eliseo <jose.eliseo.gomez@gmail.com>
2018-07-03 02:10:48 -0600

    Integracion de juego aleatorio

--- MilesToMeters/LetsPlayController.swift
+++ MilesToMeters/LetsPlayController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class LetsPlayController: UIViewController {
+    //
+    //properties
+    @IBOutlet var txtFingers: UITextField!
+    @IBOutlet var cpuFingers: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,4 +37,62 @@ class LetsPlayController: UIViewController {
     }
     */
 
+    @IBAction func guessFingers(_ sender: Any) {
+        var aleatorio = ""
+
+        let num:Int = Int(arc4random_uniform(5))
+        
+        if txtFingers.text != "" {
+            
+            switch (num){
+            case 1:
+                aleatorio = "☝🏻"
+                break
+            case 2:
+                aleatorio = "✌🏻"
+                break
+            case 3:
+                aleatorio = "3️⃣"
+                break
+            case 4:
+                aleatorio = "4️⃣"
+                break
+            case 5:
+                aleatorio = "🖐🏻"
+                break
+            default:
+                aleatorio = "0"
+                break
+            }
+            cpuFingers.text = aleatorio
+            
+            
+            if Int(txtFingers.text!)! == num{
+                let alertControler : UIAlertController = UIAlertController(title: "Exito 👏🏻", message: "Adivinaste", preferredStyle: .alert)
+                
+                let closeAlert : UIAlertAction = UIAlertAction(title: "Cerrar", style: .default, handler: nil)
+                
+                alertControler.addAction(closeAlert)
+                
+                present(alertControler, animated: true, completion: nil)
+            }else{
+                let alertControler : UIAlertController = UIAlertController(title: "☹️", message: "Sigue intentando!!", preferredStyle: .alert)
+                
+                let closeAlert : UIAlertAction = UIAlertAction(title: "Cerrar", style: .default, handler: nil)
+                
+                alertControler.addAction(closeAlert)
+                
+                present(alertControler, animated: true, completion: nil)
+            }
+            
+        }else{
+            let alertControler : UIAlertController = UIAlertController(title: "Error", message: "Favor ingresar un valor", preferredStyle: .alert)
+            
+            let closeAlert : UIAlertAction = UIAlertAction(title: "Cerrar", style: .default, handler: nil)
+            
+            alertControler.addAction(closeAlert)
+            
+            present(alertControler, animated: true, completion: nil)
+        }
+    }
 }
